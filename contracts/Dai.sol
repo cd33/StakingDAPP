302a9cb0a7bb512290bd810d0e9af1572cf8608a
cd33 <charles.diallo@ynov.com>
2021-10-03 16:57:07 +0200

    make it better

--- contracts/Dai.sol
+++ contracts/Dai.sol
@@ -1,16 +1,10 @@
 // SPDX-License-Identifier: MIT 
-// pragma solidity 0.8.9;
-// import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
- 
-// contract Dai is ERC20 {
-// //  constructor() ERC20('Dai Stablecoin', 'DAI') {}
-//  constructor(uint256 initialSupply) ERC20('Dai Stablecoin', 'DAI') {
-//     _mint(msg.sender, initialSupply);
-//  }
-// }
 pragma solidity 0.8.9;
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
  
 contract Dai is ERC20 {
- constructor() ERC20('Dai Stablecoin', 'DAI') {}
+//  constructor() ERC20('Dai Stablecoin', 'DAI') {}
+ constructor(uint256 initialSupply) ERC20('Dai Stablecoin', 'DAI') {
+    _mint(msg.sender, initialSupply);
+ }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,15 +1,15 @@
 const Dai = artifacts.require("Dai");
 const Bibscoin = artifacts.require("Bibscoin");
-const ChainlinkDaiTests = artifacts.require("ChainlinkDaiTests");
+// const ChainlinkDaiTests = artifacts.require("ChainlinkDaiTests");
 const BibsStaking = artifacts.require("BibsStaking");
 
-const DaiAddress = "0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa"; //DAI
-const ChainlinkAddress = "0x22B58f1EbEDfCA50feF632bD73368b2FdA96D541";
+// const tokenAddress = "0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa"; //DAI
+const chainlinkAddress = "0x22B58f1EbEDfCA50feF632bD73368b2FdA96D541"
 
 module.exports = async function (deployer, _network, accounts) {
-  // const dai = await Dai.at(DaiAddress) // DELETE
-  // await deployer.deploy(Dai, "5000000000000000000000");
-  // const dai = await Dai.deployed();  
+  // const dai = await deployer.at()
+  await deployer.deploy(Dai, "5000000000000000000000");
+  const dai = await Dai.deployed();
 
   await deployer.deploy(Bibscoin);
   const bibscoin = await Bibscoin.deployed();
@@ -18,15 +18,8 @@ module.exports = async function (deployer, _network, accounts) {
   // const chainlinkDaiTests = await ChainlinkDaiTests.deployed();
 
   // await deployer.deploy(BibsStaking, dai.address, bibscoin.address, chainlinkDaiTests.address);
-  await deployer.deploy(BibsStaking, DaiAddress, bibscoin.address, ChainlinkAddress);
-
-  // await dai.transfer(accounts[0], 100, { from: accounts[0] });
-
-  // const contractBeforeBalance = await dai.balanceOf(BibsStaking.address);
-  // const accountBeforeBalance = await dai.balanceOf(accounts[0]);
-  // console.log("contractBeforeBalance: " + contractBeforeBalance.toString());
-  // console.log("accountBeforeBalance: " + accountBeforeBalance.toString());
+  await deployer.deploy(BibsStaking, dai.address, bibscoin.address, chainlinkAddress);
 
   // 1000 dai
-  // await dai.transfer(accounts[1], "1000000000000000000000");
+  await dai.transfer(accounts[1], "1000000000000000000000");
 };
